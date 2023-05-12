058710880fea3000f19cd8509a9ca6136d1b04dd
glauco-dev <icaroglauco@email.com>
2023-05-11 23:25:25 -0300

    changed fingerprint behaviour

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     // Add options here
+    fingerprint:{
+      enabled: false
+    }
   });
+
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
