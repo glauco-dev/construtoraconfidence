9e56c2bec571b999e10bf30a4fc236e8531af419
glauco-dev <icaroglauco@email.com>
2023-05-20 13:47:20 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   .gitignore
    	modified:   app/components/contato.hbs
    	new file:   app/components/contato.js
    	new file:   functions/send-contact-email/send-contact-email.js
    	modified:   netlify.toml
    	modified:   package-lock.json
    	modified:   package.json

---
+++ app/components/contato.js
@@ -0,0 +1,39 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+export default class ContatoForm extends Component {
+  @tracked subject = '';
+  @tracked message = '';
+  @tracked contactName = '';
+  @tracked contactEmail = '';
+
+  get disableSubmit() {
+    return (
+      !this.contactEmail.length ||
+      !this.contactName.length ||
+      !this.message.length ||
+      !this.subject.length
+    );
+  }
+
+  @action async onSubmit(e) {
+    e.preventDefault();
+    let { subject, message, contactEmail, contactName } = this;
+    const response = await fetch('/.netlify/functions/send-contact-email', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ subject, message, contactEmail, contactName }),
+    });
+    if (response.ok) {
+      this.contactEmail = '';
+      this.contactName = '';
+      this.message = '';
+      this.subject = '';
+      window.alert('Sua mensagem foi enviada!');
+      console.log('ok');
+    } else {
+      console.log('err');
+    }
+  }
+}
---
+++ functions/send-contact-email/send-contact-email.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+const formData = require('form-data');
+const Mailgun = require('mailgun.js');
+const mailgun = new Mailgun(formData);
+const {
+  MAILGUN_API_KEY,
+  MAILGUN_DOMAIN,
+  FROM_EMAIL_ADDRESS,
+  CONTACT_TO_EMAIL_ADDRESS,
+} = process.env;
+
+exports.handler = async (event) => {
+  const mg = mailgun.client({
+    username: 'api',
+    key: MAILGUN_API_KEY,
+  });
+
+  const data = JSON.parse(event.body);
+  if (
+    !data.subject ||
+    !data.message ||
+    !data.contactName ||
+    !data.contactEmail
+  ) {
+    return { statusCode: 422, body: 'Name, email, and message are required.' };
+  }
+
+  mg.messages.create(MAILGUN_DOMAIN, {
+    from: `Confidence Website <${FROM_EMAIL_ADDRESS}>`,
+    to: [CONTACT_TO_EMAIL_ADDRESS],
+    'h:Reply-To': data.contactEmail,
+    subject: `Novo contato: ${data.subject}`,
+    text: `Name: ${data.contactName}\nEmail: ${data.contactEmail}\nMessage:\n${data.message}`,
+  });
+
+  return { statusCode: 200, body: 'foi' };
+};
