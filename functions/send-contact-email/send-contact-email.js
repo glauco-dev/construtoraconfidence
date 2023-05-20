56a3aa13938c042d35cabb273ad9765fd0ec5847
glauco-dev <icaroglauco@email.com>
2023-05-20 14:36:10 -0300

    Applying promise messages

--- functions/send-contact-email/send-contact-email.js
+++ functions/send-contact-email/send-contact-email.js
@@ -26,17 +26,24 @@ exports.handler = async (event) => {
     return { statusCode: 422, body: 'Name, email, and message are required.' };
   }
 
-  console.log(MAILGUN_API_KEY,
+  console.log(
+    MAILGUN_API_KEY,
     MAILGUN_DOMAIN,
     FROM_EMAIL_ADDRESS,
-    CONTACT_TO_EMAIL_ADDRESS, data)
+    CONTACT_TO_EMAIL_ADDRESS,
+    data
+  );
 
-  mg.messages.create(MAILGUN_DOMAIN, {
-    from: `Confidence Website <${FROM_EMAIL_ADDRESS}>`,
-    to: [CONTACT_TO_EMAIL_ADDRESS],
-    subject: `Novo contato: ${data.subject}`,
-    text: `Name: ${data.contactName}\nEmail: ${data.contactEmail}\nMessage:\n${data.message}`,
-  });
+  mg.messages
+    .create(MAILGUN_DOMAIN, {
+      from: `Confidence Website <${FROM_EMAIL_ADDRESS}>`,
+      to: [CONTACT_TO_EMAIL_ADDRESS],
+      'h:Reply-To': data.contactEmail,
+      subject: `Novo contato: ${data.subject}`,
+      text: `Name: ${data.contactName}\nEmail: ${data.contactEmail}\nMessage:\n${data.message}`,
+    })
+    .then((msg) => console.log(msg)) // logs response data
+    .catch((err) => console.log(err)); // logs any error;
 
   return { statusCode: 200, body: 'foi' };
 };
